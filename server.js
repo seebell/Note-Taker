948fcb3b5c03de8c72cb40ee9f6e18e335ec761b
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-06-11 14:59:21 -0400

    code for post request to save notes

--- server.js
+++ server.js
@@ -19,3 +19,24 @@ app.get("/", (req, res) => {
 app.get("/notes", function(req,res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+app.post("/api/notes", async function (req, res) {
+    console.log(`req.body`);
+    console.log(req.body);
+
+    let file = await fs.readFile("./db/db.json", "utf8")
+
+    console.log("got the info from db.json")
+    console.log(file)
+    const newFile = JSON.parse(file);
+    const lastId = newFile[newFile.length - 1].lastId
+    req.body.id = lastId + 1 || 1
+    newFile.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(newFile))
+        .then(function () {
+            console.log("The note has been written to db.json")
+
+            res.json(req.body);
+        })
+        .catch(err => console.log(err))
+})
