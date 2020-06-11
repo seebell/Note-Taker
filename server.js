7077e723c1592ede530ce56291411daf2c4eec75
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-06-11 14:50:19 -0400

    code to set up express and handling data parsing

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const util = require('util');
 fs.readFile = util.promisify(fs.readFile);
 fs.writeFile = util.promisify(fs.writeFile);
 var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
