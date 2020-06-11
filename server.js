c59b3dbebdd873c912e33d0782cd72b303c8bd01
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-06-11 14:52:49 -0400

    wrote the code for setting up routes

--- server.js
+++ server.js
@@ -11,3 +11,11 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req,res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
